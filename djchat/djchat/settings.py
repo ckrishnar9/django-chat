932906b97d3e28610cfc3d60cde7e9fb8ca097d7
ckrishnar9 <ckrishnar9@gmail.com>
2024-04-26 15:44:38 +0530

    environment variables management

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e=bo#8-so@3ao6xml&j^7d!w!8)x-0dh4#nhd@82r7+vf)a7ny"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
+
+# SECURITY WARNING: define the correct hosts in production!
 
 ALLOWED_HOSTS = []
 
