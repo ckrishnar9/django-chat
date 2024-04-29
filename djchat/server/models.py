4202f96b1388b132be3b5e5e8256af6758a97dcd
ckrishnar9 <ckrishnar9@gmail.com>
2024-04-29 20:50:18 +0530

    filtering servers by user and serverid

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,4 +1,6 @@
 from typing import Iterable
+
+
 from django.conf import settings
 from django.db import models
 
@@ -6,28 +8,40 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField(blank=True, null=True)
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class Server(models.Model):
     name = models.CharField(max_length=20)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='server_owner')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='server_category')
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="server_category"
+    )
     description = models.CharField(max_length=200, blank=True, null=True)
-    member = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='server_member')
-    
+    member = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="server_member"
+    )
+
     def __str__(self):
-        return self.name
-    
+        return f"{self.name} ID - {self.id}"
+
+
 class Channel(models.Model):
     name = models.CharField(max_length=20)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='channel_owner')
-    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='channel_server')
-    
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
+    )
+    server = models.ForeignKey(
+        Server, on_delete=models.CASCADE, related_name="channel_server"
+    )
+
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
         super(Channel, self).save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.name
