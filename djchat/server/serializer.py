ebfa0ca34d02feeb188eecdec41b4bfc9d87e791
ckrishnar9 <ckrishnar9@gmail.com>
2024-05-02 11:05:25 +0530

    number of members filter.

--- djchat/server/serializer.py
+++ djchat/server/serializer.py
@@ -1,12 +1,31 @@
 from rest_framework import serializers
-from .models import Category, Server
 
-# class CategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = '__all__'
+from .models import Category, Channel, Server
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = "__all__"
+
 
 class ServerSerializer(serializers.ModelSerializer):
+    num_members = serializers.SerializerMethodField()
+
+    channel_server = ChannelSerializer(many=True)
+
     class Meta:
         model = Server
-        fields = '__all__'
+        exclude = ("member",)
+        
+    def get_num_members(self, obj):
+        if hasattr(obj, "num_members"):
+            return obj.num_members
+        return None
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        num_members = self.context.get("with_num_members", False)
+        if not num_members:
+            data.pop("num_members", None)
+        return data
